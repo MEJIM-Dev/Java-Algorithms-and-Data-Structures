a9dc15ca5b0ba24b4a8acd5e606a53c5b912d093
Taskmastarh <taskmastarh@gmail.com>
2022-08-12 23:32:22 +0100

    updated fibonacci sequence to blazing fast levels

--- src/Algorithms/FibunachiSequence/FibunachiSequence.java
+++ src/Algorithms/FibunachiSequence/FibunachiSequence.java
@@ -1,6 +1,10 @@
 package Algorithms.FibunachiSequence;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FibunachiSequence {
+    static Map map = new HashMap();
 
     public static int FBSubtractionBackwards(int num){
         if(num>=3) {
@@ -13,9 +17,17 @@ public class FibunachiSequence {
         return num*(num-1);
     }
 
-    public static int FBAdditionForward(int num){
-        if(num<=1) return 1;
-        return num+FBAdditionForward(num-1);
+    public static long FBAdditionForward(int num){
+        if(num<=1) {
+            map.put(num, num-1);
+            return num;
+        }
+        if(map.containsKey(num)){
+            return (long)map.get(num);
+        }
+        long answer =  FBAdditionForward(num-1)+FBAdditionForward(num-2);
+        map.put(num, answer);
+        return answer;
     }
 
 //    public static int FBRecursive(int num){
--- src/Algorithms/Main.java
+++ src/Algorithms/Main.java
@@ -2,9 +2,12 @@ package Algorithms;
 
 import Algorithms.LowestPossibleIndex.LowestPossibleIndex;
 
+import static Algorithms.FibunachiSequence.FibunachiSequence.FBAdditionForward;
+
 public class Main {
     public static void main(String[] args) {
         int[] arg = {0,1,0,3,4};
-        System.out.println(LowestPossibleIndex.BruteForceSolution(arg));
+        System.out.println(FBAdditionForward(120));
+//        System.out.println(LowestPossibleIndex.BruteForceSolution(arg));
     }
 }
